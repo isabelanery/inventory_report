6c619e108ce4053c287fba87a2c1557725fc64fd
Isabela Nery <isabelanery.r@gmail.com>
2022-10-29 19:19:31 -0300

    feat: test_cria_produto

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,15 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1, 'Café', 'Baggio', '23/10/22', '23/01/23', 131313, 'em local fresco'
+    )
+
+    assert product.id == 1
+    assert product.nome_do_produto == 'Café'
+    assert product.nome_da_empresa == 'Baggio'
+    assert product.data_de_fabricacao == '23/10/22'
+    assert product.data_de_validade == '23/01/23'
+    assert product.numero_de_serie == '131313'
+    assert product.instrucoes_de_armazenamento == 'em local fresco'
