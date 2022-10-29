6cd0b671f464e4ebd233c85ea831e076b79bbcf3
Isabela Nery <isabelanery.r@gmail.com>
2022-10-29 19:37:19 -0300

    feat: test_relatorio_produto

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,22 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    nome_do_produto = 'Café'
+    nome_da_empresa = 'Baggio'
+    data_de_fabricacao = '23/10/22'
+    data_de_validade = '23/01/23'
+    instrucoes_de_armazenamento = 'em local fresco'
+    numero_de_serie = 131313
+
+    report = (f"O produto {nome_do_produto}"
+        f" fabricado em {data_de_fabricacao}"
+        f" por {nome_da_empresa} com validade"
+        f" até {data_de_validade}"
+        f" precisa ser armazenado {instrucoes_de_armazenamento}.")
+
+    product = Product(
+        13, nome_do_produto, nome_da_empresa, data_de_fabricacao, data_de_validade, numero_de_serie, instrucoes_de_armazenamento
+    )
+
+    assert repr(product) == report
